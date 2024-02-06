a6d462d6dd8246334c23cb825a956a52224151b1
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2024-02-06 13:40:34 -0700

    Add get request for single drink via id

--- drinks/views.py
+++ drinks/views.py
@@ -1,10 +1,37 @@
 from django.http import JsonResponse
 from .models import Drink
 from .serializers import DrinkSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 
+@api_view(['GET', 'POST']) # Decorator - can set request types
 def drink_list(request):
-    drinks = Drink.objects.all()
-    serializer = DrinkSerializer(drinks, many=True)
-    return JsonResponse({"drinks":serializer.data}, safe=False)
+    if request.method == 'GET':
+        drinks = Drink.objects.all()
+        serializer = DrinkSerializer(drinks, many=True)
+        return JsonResponse({"drinks":serializer.data})
+      
+    if request.method == 'POST':
+        serializer = DrinkSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
     
+@api_view(['GET', 'PUT', 'DELETE'])    
+def drink_detail(request, id):
+    # Drink.objects.get(pk='id') # pk means primary key
+
+    try:
+        drink = Drink.objects.get(pk=id)
+    except Drink.DoesNotExist:
+        return Response(stats=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = DrinkSerializer(drink)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        pass
+    elif request.method == 'DELETE':
+        pass
